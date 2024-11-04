b040eaf29da496790437fe331681535d8b838c3e
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-04 11:03:35 +0100

    refactor: classroom refactor

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -24,15 +24,17 @@ console.log(fourthTeacher);
 teachers[4] = "Patrick";
 console.log(teachers[4]);
 
+// OR
+teachers.splice(4, 1, "Patrick");
+console.log(teachers);
+
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
-console.log(lastTeacher);
-console.log(teachers);
+console.log(lastTeacher, teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers.shift();
-console.log(firstTeacher);
-console.log(teachers);
+console.log(firstTeacher, teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push("Vanessa");
@@ -40,6 +42,10 @@ console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 teachers.unshift("Sarah");
+console.log(teachers);
+
+//OR
+teachers.splice(0, 0, "Sarah");
 console.log(teachers);
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,8 +10,18 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.reverse();
+const reversedTeachers = teachers.slice().reverse();
 console.log(reversedTeachers);
+
+//OR 
+const revTeachers = [];
+for (let i = teachers.length - 1; i >= 0; i--) {
+  const curTeacher = teachers[i];
+  revTeachers.push(curTeacher);
+}
+
+console.log(revTeachers);
+console.log(teachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
